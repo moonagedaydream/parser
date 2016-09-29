0970f9a30d3ba304dfb7fb6b3cecf265798cbc35
LiloS <Lilija146@yandex.ru>
2016-09-29 18:13:08 +0300

    Added test for saving txt after parsing. Split 0 size document test to 2 (size = 0, size !=0)

--- ParserTest/MicrosoftWordParserTest.cs
+++ ParserTest/MicrosoftWordParserTest.cs
@@ -13,21 +13,50 @@ namespace ParserTest
   [TestFixture]
   public class MicrosoftWordParserTest
   {
-    #region Private fields
+   #region Private fields
     private string pathToEmptyDoc = Path.Combine(Environment.CurrentDirectory, @"DocTest\", "Empty.docx");
-    private string pathToEmptyDocText = Path.Combine(Environment.CurrentDirectory, @"ResultDocuments\", "Empty.docx");
+    private string pathToEmptyDocText = Path.Combine(Environment.CurrentDirectory, @"ResultDocuments\", "Empty.txt");
+    private string pathToEmpty0SizeDoc = Path.Combine(Environment.CurrentDirectory, @"DocTest\", "Empty0Size.docx");
     #endregion
 
+    #region Public test methods
+    /// <summary>
+    /// Check parser saves text to txt file.
+    /// </summary>
     [Test]
-    public void EmptyDoc()
+    public void CanSaveText()
     {
+      File.Delete(pathToEmptyDocText);
       Parser parser = new MicrosoftWordParser();
       parser.Parse(pathToEmptyDoc);
       parser.SaveTextToFile(pathToEmptyDocText);
 
-      string text = System.IO.File.ReadAllText(pathToEmptyDocText);
-
-      Assert.AreEqual(string.Empty, text);
+      Assert.IsTrue(File.Exists(pathToEmptyDocText), "TXT file with parsed text was not created.");
     }
+
+    /// <summary>
+    /// Parse empty docx with size = 0 (right click -> create new docx).
+    /// </summary>
+    [Test]
+    public void Empty0SizeDoc()
+    {
+      Parser parser = new MicrosoftWordParser();
+      parser.Parse(pathToEmpty0SizeDoc);
+
+      Assert.AreEqual(string.Empty, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse empty doc with size !=0 (right click -> create new docx -> open -> save).
+    /// </summary>
+    [Test]
+    public void OpenedEmptyDoc()
+    {
+      Parser parser = new MicrosoftWordParser();
+      parser.Parse(pathToEmptyDoc);
+
+      Assert.AreEqual(string.Empty, parser.Text);
+    }
+    #endregion
   }
 }
