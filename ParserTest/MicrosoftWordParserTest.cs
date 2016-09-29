0db99b6ed1a5503ba6df5a9404c99fb962b079ba
LiloS <Lilija146@yandex.ru>
2016-09-29 17:11:18 +0300

    Added unit tests project.

---
+++ ParserTest/MicrosoftWordParserTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ParserLibrary;
+using ParserLibrary.DocParsers;
+
+namespace ParserTest
+{
+  [TestFixture]
+  public class MicrosoftWordParserTest
+  {
+    #region Private fields
+    private string pathToEmptyDoc = Path.Combine(Environment.CurrentDirectory, @"DocTest\", "Empty.docx");
+    private string pathToEmptyDocText = Path.Combine(Environment.CurrentDirectory, @"ResultDocuments\", "Empty.docx");
+    #endregion
+
+    [Test]
+    public void EmptyDoc()
+    {
+      Parser parser = new MicrosoftWordParser();
+      parser.Parse(pathToEmptyDoc);
+      parser.SaveTextToFile(pathToEmptyDocText);
+
+      string text = System.IO.File.ReadAllText(pathToEmptyDocText);
+
+      Assert.AreEqual(string.Empty, text);
+    }
+  }
+}
