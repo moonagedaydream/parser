7b4e9f2b76de08bd1a2849ac7316882c841bf44a
LiloS <Lilija146@yandex.ru>
2016-09-30 20:18:51 +0300

    Added unit tests for all docx parsers

--- ParserTest/DocXParserTest.cs
+++ ParserTest/DocXParserTest.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using ParserLibrary;
 using ParserLibrary.DocParsers;
@@ -13,12 +11,6 @@ namespace ParserTest
   [TestFixture]
   public class DocXParserTest
   {
-    #region Private fields
-    private string pathToEmptyDoc = Path.Combine(Environment.CurrentDirectory, @"DocTest\", "Empty.docx");
-    private string pathToEmptyDocText = Path.Combine(Environment.CurrentDirectory, @"ResultDocuments\", "Empty.txt");
-    private string pathToEmpty0SizeDoc = Path.Combine(Environment.CurrentDirectory, @"DocTest\", "Empty0Size.docx");
-    #endregion
-
     #region Public test methods
     /// <summary>
     /// Check parser saves text to txt file.
@@ -26,12 +18,12 @@ namespace ParserTest
     [Test]
     public void CanSaveText()
     {
-      File.Delete(pathToEmptyDocText);
+      File.Delete(Constants.pathToEmptyDocText);
       Parser parser = new DocXParser();
-      parser.Parse(pathToEmptyDoc);
-      parser.SaveTextToFile(pathToEmptyDocText);
+      parser.Parse(Constants.pathToEmptyDoc);
+      parser.SaveTextToFile(Constants.pathToEmptyDocText);
 
-      Assert.IsTrue(File.Exists(pathToEmptyDocText), "TXT file with parsed text was not created.");
+      Assert.IsTrue(File.Exists(Constants.pathToEmptyDocText), "TXT file with parsed text was not created.");
     }
 
     /// <summary>
@@ -41,9 +33,9 @@ namespace ParserTest
     public void Empty0SizeDoc()
     {
       Parser parser = new DocXParser();
-      parser.Parse(pathToEmpty0SizeDoc);
+      parser.Parse(Constants.pathToEmpty0SizeDoc);
 
-      Assert.AreEqual(string.Empty, parser.Text);
+      Assert.AreEqual(Constants.emptyDocExpectedResult, parser.Text);
     }
 
     /// <summary>
@@ -53,9 +45,182 @@ namespace ParserTest
     public void OpenedEmptyDoc()
     {
       Parser parser = new DocXParser();
-      parser.Parse(pathToEmptyDoc);
+      parser.Parse(Constants.pathToEmptyDoc);
 
-      Assert.AreEqual(string.Empty, parser.Text);
+      Assert.AreEqual(Constants.emptyDocExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with .doc extension.
+    /// </summary>
+    [Test]
+    public void DocDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToDocDoc);
+
+      Assert.AreEqual(Constants.docDocExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with all English letters.
+    /// </summary>
+    [Test]
+    public void EnglishLettersDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToEnglishLettersDoc);
+
+      Assert.AreEqual(Constants.EnglishLettersExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with all numbers.
+    /// </summary>
+    [Test]
+    public void NumbersDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToNumbersDoc);
+
+      Assert.AreEqual(Constants.numbersExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with all Russian letters.
+    /// </summary>
+    [Test]
+    public void RussianLettersDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToRussianLettersDoc);
+
+      Assert.AreEqual(Constants.RussianLettersExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with special characters.
+    /// </summary>
+    [Test]
+    public void SpecialCharactersDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToSpecialCharactersDoc);
+
+      Assert.AreEqual(Constants.specialCharactersExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with columns.
+    /// </summary>
+    [Test]
+    public void WithColumnsDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithColumnsDoc);
+      
+      Assert.AreEqual(Constants.withColumnsExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with links.
+    /// </summary>
+    [Test]
+    public void WithLinkDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithLinkDoc);
+      
+      Assert.AreEqual(Constants.withLinkExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with contents.
+    /// </summary>
+    [Test]
+    public void WithContentsDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithContentsDoc);
+      
+      Assert.AreEqual(Constants.withContentsExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with footnotes.
+    /// </summary>
+    [Test]
+    public void WithFootnotesDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithFootnotesDoc);
+      
+      Assert.AreEqual(Constants.withFootnotesResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with formulas.
+    /// </summary>
+    [Test]
+    public void WithFormulasDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithFormulasDoc);
+      
+      Assert.AreEqual(Constants.withFormulasExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with numeration.
+    /// </summary>
+    [Test]
+    public void WithNumerationDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithNumerationDoc);
+      
+      Assert.AreEqual(Constants.withNumerationExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with picture.
+    /// </summary>
+    [Test]
+    public void WithPictureDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithPictureDoc);
+      
+      Assert.AreEqual(Constants.withPictureExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse doc with table.
+    /// </summary>
+    [Test]
+    public void WithTableDoc()
+    {
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToWithTableDoc);
+
+      Assert.AreEqual(Constants.withTableExpectedResult, parser.Text);
+    }
+
+    /// <summary>
+    /// Parse huge doc.
+    /// </summary>
+    [Test]
+    public void HugeDoc()
+    {
+      Stopwatch stopwatch = new Stopwatch();
+      stopwatch.Start();
+
+      Parser parser = new DocXParser();
+      parser.Parse(Constants.pathToHugeDocumentDoc);
+      parser.SaveTextToFile(Constants.pathToEmptyDocText);
+      stopwatch.Stop();
+
+      Assert.Less(stopwatch.Elapsed, TimeSpan.FromSeconds(2));
     }
     #endregion
   }
